c1433676809a49b22802ca105ea4e9cb389a66fa
AFCMS <afcm.contact@gmail.com>
2024-04-06 17:55:11 +0200

    Cleanup

--- src/fetch_github.ts
+++ src/fetch_github.ts
@@ -4,13 +4,11 @@ import {GraphqlResponseError} from "@octokit/graphql"
 
 export default async function () {
     try {
-        // Your token.
         const octokit = new Octokit({auth: process.env.GITHUB_TOKEN})
 
         const now = new Date()
-        // Generate ISO-8601 UTC date strings, e.g., "2024-04-01T00:00:00.000Z" in case of no input.
-        const startTime = process.argv[3]
-                ?? new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), 1)).toISOString()
+
+        const startTime = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), 1)).toISOString()
         const endTime = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth() + 1, 0, 23, 59, 59, 999)).toISOString()
 
         const response: GraphQlQueryResponseData = await octokit.graphql(`
@@ -24,8 +22,7 @@ export default async function () {
         `, {
             startTime, endTime
         })
-        console.log('Start Time:', startTime)
-        console.log('End Time:', endTime)
+
         console.log(`Commits :`, response.viewer.contributionsCollection.totalCommitContributions)
     } catch (error) {
         if (error instanceof GraphqlResponseError) {
